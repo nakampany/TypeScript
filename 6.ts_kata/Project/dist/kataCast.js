76b7d7b6612a2dd1396c8553860dede7c5c77f19
nakampany <nakajima.shota@hrbrain.co.jp>
2023-06-10 16:23:08 +0900

    hoge

---
+++ 6.ts_kata/Project/dist/kataCast.js
@@ -0,0 +1,6 @@
+"use strict";
+// 型キャスト
+// ! はnullを許容しないという意味
+const paragraph = document.querySelector('message');
+const input = document.querySelector('input');
+const input1 = document.querySelector('input');
